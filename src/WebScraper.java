408eb9e1f7d8066aa4b728d67f44720773345d6d
renaj <kanegae2@illinois.edu>
2018-02-07 14:45:03 -0600

    Initial project commit and push.

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,42 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
 public class WebScraper {
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+    public static int wordCount(String url, String word) {
+        String paragraph = urlToString(url);
+        String newWord = word.toLowerCase();
+        int count = 0;
+        while (paragraph.length() >= 0) {
+            int index = paragraph.indexOf(word);
+            paragraph = paragraph.substring(index);
+            count++;
+        }
+        while (paragraph.length() >= 0) {
+            int index = paragraph.indexOf(newWord);
+            paragraph = paragraph.substring(index);
+            count++;
+        }
+        return count;
+
+    }
     public static void main(String[] unused) {
-        System.out.println("Run");
+        System.out.println(wordCount("http://erdani.com/tdpl/hamlet.txt", "Hamlet"));
     }
 }
